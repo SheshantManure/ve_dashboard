b0bdeb3f136749f212783570c1697df629b9aeb5
Sheshant-Manure <msheshant1997@gmail.com>
2024-09-15 20:58:54 +0530

    welcome page rename camelCase

--- src/views/pages/WelcomePage/WelcomePage.jsx
+++ src/views/pages/WelcomePage/WelcomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import style from "./WelcomePage.module.scss";
+import style from "./welcomePage.module.scss";
 import Navbar from "../../components/Navbar/Navbar";
 import VeLogo from "../../components/VeLogo/VeLogo";
 import Cards from "./cards";
@@ -12,123 +12,113 @@ import LoginModal from "../../components/loginModal/loginModal";
 import Hamburger from "../../../assets/svgs/mobileViewIcons/hamburger";
 
 const WelcomePage = () => {
-    const [toggleSignupSidebar, setToggleSignupSidebar] = useState(false);
-    const [loginModalToggle, setloginModalToggle] = useState(false);
-    const [isLargeScreen, setIsLargeScreen] = useState(
-        window.innerWidth >= 500
-    );
+  const [toggleSignupSidebar, setToggleSignupSidebar] = useState(false);
+  const [loginModalToggle, setloginModalToggle] = useState(false);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 500);
 
-    const promptInputRef = useRef(null);
+  const promptInputRef = useRef(null);
 
-    // Function to handle window resize
-    const handleResize = () => {
-        setIsLargeScreen(window.innerWidth >= 500);
+  // Function to handle window resize
+  const handleResize = () => {
+    setIsLargeScreen(window.innerWidth >= 500);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.ctrlKey && e.key.toLowerCase() === "l") {
+      setloginModalToggle(true);
+    }
+  };
+
+  useEffect(() => {
+    promptInputRef.current.focus();
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("keydown", handleKeyDown);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
     };
+  }, []);
+  return (
+    <div className={style.container}>
+      {toggleSignupSidebar && (
+        <SignupSidebar
+          closeSignupSidebar={() => setToggleSignupSidebar(false)}
+          openLoginModal={() => setloginModalToggle(true)}
+        />
+      )}
+      {loginModalToggle && (
+        <LoginModal closeLoginModal={() => setloginModalToggle(false)} />
+      )}
 
-    const handleKeyDown = (e) => {
-        if (e.ctrlKey && e.key.toLowerCase() === "l") {
-            setloginModalToggle(true);
-        }
-    };
-
-    useEffect(() => {
-        promptInputRef.current.focus();
-        // Add event listener for window resize
-        window.addEventListener("resize", handleResize);
-        window.addEventListener("keydown", handleKeyDown);
-
-        // Remove event listener on component unmount
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-    return (
-        <div className={style.container}>
-            {toggleSignupSidebar && (
-                <SignupSidebar
-                    closeSignupSidebar={() => setToggleSignupSidebar(false)}
-                    openLoginModal={() => setloginModalToggle(true)}
-                />
-            )}
-            {loginModalToggle && (
-                <LoginModal
-                    closeLoginModal={() => setloginModalToggle(false)}
-                />
-            )}
-
-            <div className={style.veLogoStyles}>
-                <VeLogo />
-                {!isLargeScreen && (
-                    <div className={style.hamburger}>
-                        <Hamburger
-                            openSignupSidebar={() =>
-                                setToggleSignupSidebar(true)
-                            }
-                        />
-                    </div>
-                )}
-            </div>
-            {isLargeScreen ? (
-                <Navbar
-                    openSignupSidebar={() => setToggleSignupSidebar(true)}
-                    openLoginModal={() => setloginModalToggle(true)}
-                />
-            ) : (
-                // <div className={style.hamburger}>
-                //     <Hamburger />
-                // </div>
-                <></>
-            )}
-            <div className={style.mainContent}>
-                <div className={style.mainBox}>
-                    <div className={style.title}>
-                        <h1>
-                            Welcome to <VeLogo />
-                        </h1>
-                        <h2>AI that minds your business</h2>
-                        <h3>
-                            So you can run the world. The all-in-one tool for
-                            those who do it all.
-                        </h3>
-                    </div>
-                    <h4 className={style.gettingStarted}>
-                        Let’s get started with any of these actions!
-                    </h4>
-                    <Cards />
-                    <div className={style.refreshPrompts}>
-                        <RefreshLogo />
-                        <h4>Refresh Prompts</h4>
-                    </div>
-                    <div className={style.promptContainer}>
-                        <div className={style.box550px}>
-                            <p className={style.heading}>
-                                Or Ask me anything about your business
-                            </p>
-                            <div className={style.promptInputDiv}>
-                                <div className={style.promptTextArea}>
-                                    <StarLogo />
-                                    <textarea
-                                        ref={promptInputRef}
-                                        name="prompt"
-                                        placeholder="Hey, give me million dollar service business idea!"
-                                    ></textarea>
-                                </div>
-                                <div className={style.promptActions}>
-                                    <MicrophoneLogo />
-                                    <RightArrowLogo
-                                        className={style.rightArrowLogo}
-                                    />
-                                </div>
-                            </div>
-                            <p className={style.tagLine}>
-                                Built by Professionals for Professionals
-                            </p>
-                        </div>
-                    </div>
+      <div className={style.veLogoStyles}>
+        <VeLogo />
+        {!isLargeScreen && (
+          <div className={style.hamburger}>
+            <Hamburger openSignupSidebar={() => setToggleSignupSidebar(true)} />
+          </div>
+        )}
+      </div>
+      {isLargeScreen ? (
+        <Navbar
+          openSignupSidebar={() => setToggleSignupSidebar(true)}
+          openLoginModal={() => setloginModalToggle(true)}
+        />
+      ) : (
+        // <div className={style.hamburger}>
+        //     <Hamburger />
+        // </div>
+        <></>
+      )}
+      <div className={style.mainContent}>
+        <div className={style.mainBox}>
+          <div className={style.title}>
+            <h1>
+              Welcome to <VeLogo />
+            </h1>
+            <h2>AI that minds your business</h2>
+            <h3>
+              So you can run the world. The all-in-one tool for those who do it
+              all.
+            </h3>
+          </div>
+          <h4 className={style.gettingStarted}>
+            Let’s get started with any of these actions!
+          </h4>
+          <Cards />
+          <div className={style.refreshPrompts}>
+            <RefreshLogo />
+            <h4>Refresh Prompts</h4>
+          </div>
+          <div className={style.promptContainer}>
+            <div className={style.box550px}>
+              <p className={style.heading}>
+                Or Ask me anything about your business
+              </p>
+              <div className={style.promptInputDiv}>
+                <div className={style.promptTextArea}>
+                  <StarLogo />
+                  <textarea
+                    ref={promptInputRef}
+                    name="prompt"
+                    placeholder="Hey, give me million dollar service business idea!"
+                  ></textarea>
                 </div>
+                <div className={style.promptActions}>
+                  <MicrophoneLogo />
+                  <RightArrowLogo className={style.rightArrowLogo} />
+                </div>
+              </div>
+              <p className={style.tagLine}>
+                Built by Professionals for Professionals
+              </p>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default WelcomePage;
--- src/views/pages/WelcomePage/cards.jsx
+++ src/views/pages/WelcomePage/cards.jsx
@@ -1,144 +1,132 @@
 import React, { useState } from "react";
-import style from "./WelcomePage.module.scss";
+import style from "./welcomePage.module.scss";
 import ProposalLogo from "../../../assets/svgs/welcomePageLogos/proposalLogo";
 import LinkInBioLogo from "../../../assets/svgs/welcomePageLogos/linkInBioLogo";
 import InvoiceLogo from "../../../assets/svgs/welcomePageLogos/invoiceLogo";
 import WorkflowsLogo from "../../../assets/svgs/welcomePageLogos/workflowsLogo";
 
 const cards = [
-    {
-        description:
-            "New proposal submission tool that showcases your data Clearly and Effectively",
-        title: "Send Proposals",
-        logo: "ProposalLogo",
-    },
-    {
-        description:
-            "Make it easier for your audience to connect with all your essential content through",
-        title: "Link in Bio",
-        logo: "LinkInBioLogo",
-    },
-    {
-        description:
-            "Simplifying your billing process. Create and share professional, detailed invoices in seconds",
-        title: "Send Invoices",
-        logo: "InvoiceLogo",
-    },
-    {
-        description:
-            "From task management to client follow-ups, automate routine actions and focus on what truly matters",
-        title: "Automate Workflows",
-        logo: "WorkflowsLogo",
-    },
+  {
+    description:
+      "New proposal submission tool that showcases your data Clearly and Effectively",
+    title: "Send Proposals",
+    logo: "ProposalLogo",
+  },
+  {
+    description:
+      "Make it easier for your audience to connect with all your essential content through",
+    title: "Link in Bio",
+    logo: "LinkInBioLogo",
+  },
+  {
+    description:
+      "Simplifying your billing process. Create and share professional, detailed invoices in seconds",
+    title: "Send Invoices",
+    logo: "InvoiceLogo",
+  },
+  {
+    description:
+      "From task management to client follow-ups, automate routine actions and focus on what truly matters",
+    title: "Automate Workflows",
+    logo: "WorkflowsLogo",
+  },
 ];
 
 const Cards = () => {
-    // Create a hover state for each card (using index)
-    const [hoveredCard, setHoveredCard] = useState(null);
+  // Create a hover state for each card (using index)
+  const [hoveredCard, setHoveredCard] = useState(null);
 
-    const renderLogo = (logoName) => {
-        switch (logoName) {
-            case "ProposalLogo":
-                return <ProposalLogo />;
-            case "LinkInBioLogo":
-                return <LinkInBioLogo />;
-            case "InvoiceLogo":
-                return <InvoiceLogo />;
-            case "WorkflowsLogo":
-                return <WorkflowsLogo />;
-            default:
-                return null;
-        }
-    };
+  const renderLogo = (logoName) => {
+    switch (logoName) {
+      case "ProposalLogo":
+        return <ProposalLogo />;
+      case "LinkInBioLogo":
+        return <LinkInBioLogo />;
+      case "InvoiceLogo":
+        return <InvoiceLogo />;
+      case "WorkflowsLogo":
+        return <WorkflowsLogo />;
+      default:
+        return null;
+    }
+  };
 
-    return (
-        <div className={style.cardsContainer}>
-            {cards.map((card, index) => (
-                <div
-                    key={index}
-                    className={style.card}
-                    onMouseEnter={() => setHoveredCard(index)} // Set hovered card index
-                    onMouseLeave={() => setHoveredCard(null)} // Reset on mouse leave
+  return (
+    <div className={style.cardsContainer}>
+      {cards.map((card, index) => (
+        <div
+          key={index}
+          className={style.card}
+          onMouseEnter={() => setHoveredCard(index)} // Set hovered card index
+          onMouseLeave={() => setHoveredCard(null)} // Reset on mouse leave
+        >
+          {hoveredCard === index && ( // Render only for the hovered card
+            <svg
+              className={style.gradient}
+              xmlns="http://www.w3.org/2000/svg"
+              width="247"
+              height="168"
+              viewBox="0 0 247 168"
+              fill="none"
+            >
+              <g clipPath="url(#clip0_1141_10932)">
+                <g opacity="0.48" filter="url(#filter0_f_1141_10932)">
+                  <circle
+                    cx="-8.5"
+                    cy="-46.5"
+                    r="61.5"
+                    fill="url(#paint0_linear_1141_10932)"
+                  />
+                </g>
+              </g>
+              <defs>
+                <filter
+                  id="filter0_f_1141_10932"
+                  x="-139"
+                  y="-177"
+                  width="261"
+                  height="261"
+                  filterUnits="userSpaceOnUse"
+                  colorInterpolationFilters="sRGB"
                 >
-                    {hoveredCard === index && ( // Render only for the hovered card
-                        <svg
-                            className={style.gradient}
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="247"
-                            height="168"
-                            viewBox="0 0 247 168"
-                            fill="none"
-                        >
-                            <g clipPath="url(#clip0_1141_10932)">
-                                <g
-                                    opacity="0.48"
-                                    filter="url(#filter0_f_1141_10932)"
-                                >
-                                    <circle
-                                        cx="-8.5"
-                                        cy="-46.5"
-                                        r="61.5"
-                                        fill="url(#paint0_linear_1141_10932)"
-                                    />
-                                </g>
-                            </g>
-                            <defs>
-                                <filter
-                                    id="filter0_f_1141_10932"
-                                    x="-139"
-                                    y="-177"
-                                    width="261"
-                                    height="261"
-                                    filterUnits="userSpaceOnUse"
-                                    colorInterpolationFilters="sRGB"
-                                >
-                                    <feFlood
-                                        floodOpacity="0"
-                                        result="BackgroundImageFix"
-                                    />
-                                    <feBlend
-                                        mode="normal"
-                                        in="SourceGraphic"
-                                        in2="BackgroundImageFix"
-                                        result="shape"
-                                    />
-                                    <feGaussianBlur
-                                        stdDeviation="34.5"
-                                        result="effect1_foregroundBlur_1141_10932"
-                                    />
-                                </filter>
-                                <linearGradient
-                                    id="paint0_linear_1141_10932"
-                                    x1="-8.5"
-                                    y1="-108"
-                                    x2="-8.5"
-                                    y2="15"
-                                    gradientUnits="userSpaceOnUse"
-                                >
-                                    <stop stopColor="#BD9DFF" />
-                                    <stop offset="1" stopColor="#6055EC" />
-                                </linearGradient>
-                                <clipPath id="clip0_1141_10932">
-                                    <rect
-                                        width="247"
-                                        height="168"
-                                        fill="white"
-                                    />
-                                </clipPath>
-                            </defs>
-                        </svg>
-                    )}
-                    <p className={style.description}>{card.description}</p>
-                    <nav>
-                        <div className={style.logo}>
-                            {renderLogo(card.logo)}
-                        </div>
-                        <h3>{card.title}</h3>
-                    </nav>
-                </div>
-            ))}
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                  <feBlend
+                    mode="normal"
+                    in="SourceGraphic"
+                    in2="BackgroundImageFix"
+                    result="shape"
+                  />
+                  <feGaussianBlur
+                    stdDeviation="34.5"
+                    result="effect1_foregroundBlur_1141_10932"
+                  />
+                </filter>
+                <linearGradient
+                  id="paint0_linear_1141_10932"
+                  x1="-8.5"
+                  y1="-108"
+                  x2="-8.5"
+                  y2="15"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#BD9DFF" />
+                  <stop offset="1" stopColor="#6055EC" />
+                </linearGradient>
+                <clipPath id="clip0_1141_10932">
+                  <rect width="247" height="168" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          )}
+          <p className={style.description}>{card.description}</p>
+          <nav>
+            <div className={style.logo}>{renderLogo(card.logo)}</div>
+            <h3>{card.title}</h3>
+          </nav>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Cards;
